fa1026208834f333b3e9f48bf6ea186d249d96ba
Justin Ching <justin_ethan_l_ching@dlsu.edu.ph>
2024-10-03 20:50:41 +0800

    added directed_weighted_connect function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pythonds.graphs import Graph
+from pythonds.basic import Stack, Queue
 
 # undirected_connect Connects two nodes together (Undirected)
 # graph is the graph
@@ -7,6 +8,14 @@ from pythonds.graphs import Graph
 def undirected_connect(graph, one, two):
     graph.addEdge(one, two)
     graph.addEdge(two, one)
+
+# directed_weighted_connect Connects a node to another node (Directed from one to two)
+# graph is the graph
+# one is first node
+# two is second node
+# weight is the weight of the edge
+def directed_weighted_connect(graph, one, two, weight):
+    graph.addEdge(one, two, weight)
 
 # Main function
 def main():
